34e9a828c49bfbda7e7b2136cb9f42800725f0b6
Karen Payne <paynekaren@comcast.net>
2023-04-24 06:39:54 -0700

    local settings

--- ReducedMotionExample/readme.md
+++ ReducedMotionExample/readme.md
@@ -1,1 +1,18 @@
-﻿# About
+﻿# Tips on making a web site accessible part 2
+
+## Using the CSS reduce-motion query to prevent motion
+
+Some users experience distraction or nausea from animated content.
+
+> **Note**
+> The impact of animation on people with vestibular disorders can be quite severe. Triggered reactions include nausea, migraine headaches, and potentially needing bed rest to recover.
+
+**Vestibular Disorder**
+
+- People with [vestibular disorders](https://vestibular.org/) need control over movement triggered by interactions. Non-essential movement can trigger vestibular disorder reactions. Vestibular (inner ear) disorder reactions include distraction, dizziness, headaches and nausea.
+- Persona Quote: "Stop that extra movement! You are making me so dizzy I cannot concentrate. Now I have to turn off my computer and go lie down."
+
+Information above is from [WCAG 2.1 Techniques](https://www.w3.org/WAI/WCAG21/Techniques/css/C39)
+
+
+
---
+++ ReducedMotionExample/wwwroot/js/Application.js
@@ -0,0 +1,36 @@
+﻿var $Application = $Application || {};
+$Application = function () {
+    const motionKey = 'useReduceMotion';
+    var supportsLocalStorage = function () {
+        return typeof (Storage) !== "undefined";
+    };
+    var setMotion = function (value) {
+        if (!supportsLocalStorage()) {
+            return false;
+        }
+        localStorage.setItem(motionKey, value);
+        return true;
+    };
+
+    var getMotion = function () {
+        if (localStorage.getItem(motionKey) === null) {
+            localStorage.setItem(motionKey, false);
+        }
+
+        return localStorage.getItem(motionKey);
+    };
+
+    var prefersReducedMotion = function () {
+        // Grab the user's reduced motion setting
+        const results = window.matchMedia('(prefers-reduced-motion: reduce)');
+
+        // Check if the media query matches or is not available.
+        return !results || results.matches;
+    }
+
+    return {
+        setMotion: setMotion,
+        getMotion: getMotion,
+        prefersReducedMotion: prefersReducedMotion
+    };
+}();
